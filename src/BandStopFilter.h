22b4398ef2346c41cc1edad17a4ba3d4de1a0570
Mirco Pergreffi <mircopergreffi@gmail.com>
2021-03-17 23:54:23 +0100

    Update BandStopFilter.h

--- src/BandStopFilter.h
+++ src/BandStopFilter.h
@@ -10,10 +10,9 @@ class BandStopFilter : public Filter
 {
   private:
     float mLowCut, mHighCut;
-  
-    int mTimeNew, mTimeLast, mTimeDiff;
+
+    float mLastRaw;
     float mLowPass, mHighPass;
-    float mLowPass2, mHighPass2;
     float mBandStop;
 
   public:
@@ -21,27 +20,18 @@ class BandStopFilter : public Filter
     {
       mLowCut = freq - radius;
       mHighCut = freq + radius;
-
-      mTimeLast = 0;
       
+      mLastRaw = 0;
       mLowPass = 0;
       mHighPass = 0;
-      mLowPass2 = 0;
-      mHighPass2 = 0;
     }
 
-    float filter(float raw)
-    {
-      mTimeNew = micros();
-      mTimeDiff = mTimeNew - mTimeLast;
-      mTimeLast = mTimeNew;
-      
-      mLowPass += (raw - mLowPass)*mLowCut/mTimeDiff;
-      mLowPass2 += (mLowPass - mLowPass2)*mLowCut/mTimeDiff;
-      mHighPass = (raw - mHighPass)/mHighCut/mTimeDiff;
-      mHighPass2 = (mHighPass - mHighPass2)/mHighCut/mTimeDiff;
-      
-      mBandStop = mLowPass2 + mHighPass2;
+    float filter(float raw, float dt)
+    {      
+      mLowPass += (raw - mLowPass)*mLowCut*dt;
+      mHighPass += (raw - mLastRaw) - mHighPass*mHighCut*dt;      
+      mBandStop = mLowPass + mHighPass;
+      mLastRaw = raw;
       return mBandStop;
     }
 };
