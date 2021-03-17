606be5110319ed8f9db7315b8ca8fff0eb5edfa6
Mirco Pergreffi <mircopergreffi@gmail.com>
2021-03-17 23:51:47 +0100

    Update Filter.h

--- src/Filter.h
+++ src/Filter.h
@@ -2,10 +2,28 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+#define MICROS 1000000.0
+
 class Filter
 {
+  private:
+    int mTimeNew, mTimeLast, mTimeDiff;
+    
   public:
-    virtual float filter(float raw);
+    Filter()
+    {
+      mTimeLast = 0;
+    }
+    
+    float filter(float raw)
+    {
+      mTimeNew = micros();
+      mTimeDiff = mTimeNew - mTimeLast;
+      mTimeLast = mTimeNew;
+      return filter(raw, mTimeDiff/MICROS);
+    }
+    
+    virtual float filter(float raw, float dt);
 };
 
 #endif /* FILTER_H */
